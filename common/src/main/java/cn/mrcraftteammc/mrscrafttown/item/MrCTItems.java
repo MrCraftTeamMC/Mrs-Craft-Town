ce8f4e420442b59d9640213e4e38acaa6e71f5d9
Mr_limr233 <113236317+erha134@users.noreply.github.com>
2023-08-16 20:22:51 +0800

    Fix Creative Tab Content Registry

--- common/src/main/java/cn/mrcraftteammc/mrscrafttown/item/MrCTItems.java
+++ common/src/main/java/cn/mrcraftteammc/mrscrafttown/item/MrCTItems.java
@@ -1,6 +1,9 @@
 package cn.mrcraftteammc.mrscrafttown.item;
 
+import cn.mrcraftteammc.mrscrafttown.item.items.copper.CyanCopperItem;
+import cn.mrcraftteammc.mrscrafttown.item.items.copper.YellowCopperItem;
 import dev.architectury.registry.registries.DeferredRegister;
+import dev.architectury.registry.registries.RegistrySupplier;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.Item;
 
@@ -9,7 +12,8 @@ import static cn.mrcraftteammc.mrscrafttown.MrsCraftTownMod.MOD_ID;
 public class MrCTItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(MOD_ID, Registries.ITEM);
 
-    //public static final RegistrySupplier<Item> ZIPPED_POTATO_ITEM = ITEMS.register("zipped_potato_item", ZippedPotatoItem::new);
+    public static final RegistrySupplier<Item> CYAN_COPPER_ITEM = ITEMS.register("cyan_copper_item", CyanCopperItem::new);
+    public static final RegistrySupplier<Item> YELLOW_COPPER_ITEM = ITEMS.register("yellow_copper_item", YellowCopperItem::new);
 
     public static void register() {
         MrCTBlockItems.register();
---
+++ common/src/main/java/cn/mrcraftteammc/mrscrafttown/item/items/copper/YellowCopperItem.java
@@ -0,0 +1,10 @@
+package cn.mrcraftteammc.mrscrafttown.item.items.copper;
+
+import cn.mrcraftteammc.mrscrafttown.creativetab.MrCTCreativeTabs;
+import net.minecraft.world.item.Item;
+
+public class YellowCopperItem extends Item {
+    public YellowCopperItem() {
+        super(new Properties().stacksTo(64).arch$tab(MrCTCreativeTabs.BASE_TAB));
+    }
+}
